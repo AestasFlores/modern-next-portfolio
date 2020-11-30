13c0c16d4531d1249e83354bb02575eb9d38bc00
Kaguwo <kaguwomin@gmail.com>
2020-11-30 09:15:38 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
     <div>
       <Head>
         <title>Kaguwo's Portfolio</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href={favicon} />
       </Head>
 
