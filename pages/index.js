c5c81c12eda961560b329ee5f05d4b0c56c3e1d6
Kaguwo <kaguwomin@gmail.com>
2020-12-26 17:58:58 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Layout from '../components/layout'
 import favicon from '../public/favicon.ico'
+import { NextSeo } from 'next-seo'
 
 export default function Home() {
   return (
@@ -10,7 +11,16 @@ export default function Home() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href={favicon} />
       </Head>
-
+      
+      <NextSeo
+      openGraph={{
+        type: 'website',
+        url: 'https://www.kaguwo.js.org',
+        title: 'Kaguwo\'s Portfolio',
+        description: 'A neoterically designed portfolio, made with Next.JS and Tailwind CSS. Check out this website source on Github!',
+      }}
+    />
+        
       <Layout />
     </div>
   )
