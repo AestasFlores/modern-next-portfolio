b336da29bf07c9ef85809a1aa9e55b9cece36a72
Kaguwo <kaguwomin@gmail.com>
2021-01-17 00:09:38 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,2 +1,6 @@
-const withImages = require('next-images')
-module.exports = withImages()
+/* const withImages = require('next-images')
+module.exports = withImages() */
+
+/* Okay, so I'm not gonna use next-images module on this project furthermore. 
+I found a good solution about images with creating just a "config.js" file in general repository. 
+I learned this technique from @thisisroi, you can look his projects in Github. Most of them were amazing btw! */
