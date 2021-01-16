3df05cc995e8fb99ad62a7ee0a923907fbc954e4
Kaguwo <kaguwomin@gmail.com>
2021-01-17 00:24:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 export default {
    to: {
      image: 'https://cdn.discordapp.com/avatars/495584014934081538/c40c757524f594429d6d15b37fc484e7.png?size=1024',
+     favicon: './public/favicon.ico'
   },
 };
