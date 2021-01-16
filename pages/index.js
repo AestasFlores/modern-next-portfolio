274e319ba6e88a426f5520081cb48c909b91b17c
Kaguwo <kaguwomin@gmail.com>
2021-01-17 00:25:42 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Layout from '../components/layout'
-import favicon from '../public/favicon.ico'
+import config from '../config.js'
 import { NextSeo } from 'next-seo'
 
 export default function Home() {
@@ -9,7 +9,7 @@ export default function Home() {
       <Head>
         <title>Kaguwo's Portfolio</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="icon" href={favicon} />
+        <link rel="icon" href={config.to.favicon} />
       </Head>
       
       <NextSeo
