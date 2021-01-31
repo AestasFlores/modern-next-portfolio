fecadbc3867eeed781da3f6f24f054fe01d38fbc
Hüseyin Yalçın <kaguwomin@gmail.com>
2021-01-31 20:08:58 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 export default {
    to: {
-     image: 'https://cdn.discordapp.com/avatars/495584014934081538/ac2ed10e8375777ad38b7f9754e41522.png?size=1024',
-     favicon: './public/favicon.ico'
+     image: 'https://cdn.discordapp.com/avatars/495584014934081538/ac2ed10e8375777ad38b7f9754e41522.png?size=1024'
   },
 };
