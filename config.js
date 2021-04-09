fb0f519e16129991320ccee12e6e49d6238338e3
Hüseyin Yalçın <kaguwomin@gmail.com>
2021-04-09 17:03:01 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
    to: {
-     image: 'https://images-ext-2.discordapp.net/external/UAHyAjECWSesMBjrrpwUGlz8uS2KCMFLhdwniIt5I3s/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/495584014934081538/4fa4e4fa6a3f64613d648791ed35b721.png'
+     image: 'https://avatars.githubusercontent.com/u/60431344?v=4'
   },
 };
